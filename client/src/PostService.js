5410e9cff7587083d3acb0be8216ba3b880f2338
Am05gautam <am05gautam@gmail.com>
2022-03-31 01:39:06 +0530

    client updated

---
+++ client/src/PostService.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+
+const url = 'api/posts/';
+
+class PostService{
+    //GET Posts
+    static getPosts(){
+        return new Promise ((resolve,reject) => {
+            axios.get(url).then((res) => {
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            })
+            .catch((err)=> {
+                reject(err);
+            })
+            
+        });
+    }
+
+    //Create Post
+    static insertPost(text){
+        return axios.post(url,{
+            text
+        });
+    }
+
+
+    //Delete Post
+    static deletePost(id){
+        return axios.delete(`${url}${id}`);
+    }
+
+}
+export default PostService;
